05c5a2081bb57cb7f1296f06b835439452560f15
KushSalaskar <kushsalaskar@gmail.com>
2020-09-10 18:58:12 +0530

    node_modules finalUPDATE

---
+++ build/precache-manifest.a1e4afef3804ea7be526143ab3356650.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30299383ed03a103b3a24f9f5e314127",
+    "url": "/Corona_Virus_Tracker/index.html"
+  },
+  {
+    "revision": "8b1c97104300fb2827f8",
+    "url": "/Corona_Virus_Tracker/static/css/main.7c755789.chunk.css"
+  },
+  {
+    "revision": "49988ceac50cf62069ec",
+    "url": "/Corona_Virus_Tracker/static/js/2.c4f31dbe.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/Corona_Virus_Tracker/static/js/2.c4f31dbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b1c97104300fb2827f8",
+    "url": "/Corona_Virus_Tracker/static/js/main.82c33bda.chunk.js"
+  },
+  {
+    "revision": "aac096c5985cec805cde",
+    "url": "/Corona_Virus_Tracker/static/js/runtime-main.457bb821.js"
+  }
+]);
