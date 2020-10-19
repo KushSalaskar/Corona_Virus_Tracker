3e398a9ab2ed92705fa7e8ac9ebf3eb35cca2247
KushSalaskar <kushsalaskar@gmail.com>
2020-10-19 19:59:15 +0530

    api fix

--- build/precache-manifest.831df3e471f1c0078c2e38bf87fbc96e.js
+++ build/precache-manifest.831df3e471f1c0078c2e38bf87fbc96e.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0bd4a3dcf1a68a74d3cad17167866f88",
+    "revision": "ab3bbaa8636fa00fe9d581c988b0b72d",
     "url": "/Corona_Virus_Tracker/index.html"
   },
   {
-    "revision": "ea0baad2ab5b27bc2650",
+    "revision": "7a587903d1c66375492a",
     "url": "/Corona_Virus_Tracker/static/css/main.a2f832f3.chunk.css"
   },
   {
-    "revision": "4d2311349bcfa990f7db",
-    "url": "/Corona_Virus_Tracker/static/js/2.3c608021.chunk.js"
+    "revision": "49988ceac50cf62069ec",
+    "url": "/Corona_Virus_Tracker/static/js/2.c4f31dbe.chunk.js"
   },
   {
     "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
-    "url": "/Corona_Virus_Tracker/static/js/2.3c608021.chunk.js.LICENSE.txt"
+    "url": "/Corona_Virus_Tracker/static/js/2.c4f31dbe.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ea0baad2ab5b27bc2650",
-    "url": "/Corona_Virus_Tracker/static/js/main.b648fd4e.chunk.js"
+    "revision": "7a587903d1c66375492a",
+    "url": "/Corona_Virus_Tracker/static/js/main.097cc3cb.chunk.js"
   },
   {
     "revision": "aac096c5985cec805cde",
