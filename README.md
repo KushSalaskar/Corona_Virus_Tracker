16c03acc248c85ce4970d18f3250bc991c9b345c
KushSalaskar <kushsalaskar@gmail.com>
2020-10-19 19:49:40 +0530

    api fix

---
+++ README.md
@@ -0,0 +1,7 @@
+This is a Covid19 Tracking App
+Made by using ReactJS
+
+and Hosted on http://KushSalaskar.github.io/Corona_Virus_Tracker
+
+It visualises number of active cases in the world on a linechart
+and country wise cases on a bar graph and a piechart.
--- src/Components/api/index.js
+++ src/Components/api/index.js
@@ -15,19 +15,20 @@ export const fetchData = async (country) => {
     }
 }
 
-// export const fetchDailyData = async () => {
-//     try {
-//         const { data } = await axios.get(`${url}/daily`)
-//         const modifiedData = data.map((dailyData) => ({
-//             confirmed: dailyData.confirmed.total,
-//             deaths: dailyData.deaths.total,
-//             date: dailyData.reportDate
-//         }));
-//         return modifiedData;
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
+//to comment if api breaks again
+export const fetchDailyData = async () => {
+    try {
+        const { data } = await axios.get(`${url}/daily`)
+        const modifiedData = data.map((dailyData) => ({
+            confirmed: dailyData.confirmed.total,
+            deaths: dailyData.deaths.total,
+            date: dailyData.reportDate
+        }));
+        return modifiedData;
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 export const fetchCountries = async () => {
     try {
